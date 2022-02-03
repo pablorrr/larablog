44a7656c9027d9ed1d4650bd865663a4d35551fa
Paweł Kali <pkalisz451@gmail.com>
2022-02-03 09:29:22 +0100

    dodoanie fasady obslugiujace j  route

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 use App\Http\Resources\UserResource;
 use App\Models\User;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Route;
 
 Route::get('/user/{id}', function ($id) {
     return new UserResource(User::findOrFail($id));
