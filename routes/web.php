8444ed9684a01d254310a77e0f5d953206d9e5cb
Paweł Kali <pkalisz451@gmail.com>
2022-02-03 09:54:06 +0100

    dodoanioe komentzrza

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,9 @@ use App\Models\User;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
+
+//usage /user/id jak nie niedizla  artisan optimize
+
 Route::get('/user/{id}', function ($id) {
     return new UserResource(User::findOrFail($id));
 });
