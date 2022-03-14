d3a8079934e881626add4cbe8e80ece8bf8c2252
Paweł Kali <pkalisz451@gmail.com>
2022-03-14 14:27:37 +0100

    wzmocnienei komatarza

--- app/View/Components/AppLayout.php
+++ app/View/Components/AppLayout.php
@@ -5,7 +5,7 @@ namespace App\View\Components;
 use Illuminate\View\Component;
 
 class AppLayout extends Component
-{
+{//https://beyondco.de/blog/using-laravel-view-components
     /**
      * Get the view / contents that represents the component.
      *
