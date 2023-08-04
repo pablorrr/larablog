e3a1effcbdb1c70fd9073d1b27d47dd623c938f8
Paweł Kali <pkalisz451@gmail.com>
2023-08-04 11:18:48 +0200

    delete rite

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,7 @@
 use App\Http\Controllers\Api\ArticleController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use Illuminate\Routing\AbstractRouteCollection;
 
 /*
 |--------------------------------------------------------------------------
@@ -19,6 +20,10 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 //przed articles nalezy dopisac - api
-Route::get('articles/',[ArticleController::class,'index']);
 
-Route::get('articles/{id}',[ArticleController::class,'show']);
+Route::get('articles/index', [ArticleController::class, 'index']);
+
+Route::get('articles/show/{id}', [ArticleController::class, 'show']);
+
+//nie dziala
+Route::delete('articles/delete/{id}',[ArticleController::class,'deleteArticle']);
